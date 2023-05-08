1aeed65bfc2e4f8f4d11952a5763a6c8930d5b8d
Pearstopher <pearstopher@gmail.com>
2023-05-08 12:39:51 -0700

    updating tests, first test passes

--- backend/test/app.test.ts
+++ backend/test/app.test.ts
@@ -11,36 +11,38 @@ test("Search for a character that exists by name", async () => {
 	});
 
 	response.statusCode.should.equal(200);
+	const rj = response.json();
+	rj.length.should.equal(1);
 	//response.body.should.equal("hello");
 });
 
-test("List all users from /dbTest", async () => {
-	const response = await app.inject({
-		method: "GET",
-		url: "/dbTest",
-	});
+// test("List all users from /dbTest", async () => {
+// 	const response = await app.inject({
+// 		method: "GET",
+// 		url: "/dbTest",
+// 	});
+//
+// 	response.statusCode.should.equal(200);
+// });
 
-	response.statusCode.should.equal(200);
-});
-
-test("Creating a new user", async () => {
-	const payload = {
-		name: "Testname",
-		email: faker.internet.email(),
-		petType: "Dog",
-	};
-
-	const response = await app.inject({
-		method: "POST",
-		url: "/users",
-		payload,
-	});
-
-	response.statusCode.should.equal(200);
-	response.payload.should.not.equal(payload);
-	const resPayload = response.json();
-	resPayload.email.should.equal(payload.email);
-	resPayload.petType.should.equal("Dog");
-});
+// test("Creating a new user", async () => {
+// 	const payload = {
+// 		name: "Testname",
+// 		email: faker.internet.email(),
+// 		petType: "Dog",
+// 	};
+//
+// 	const response = await app.inject({
+// 		method: "POST",
+// 		url: "/users",
+// 		payload,
+// 	});
+//
+// 	response.statusCode.should.equal(200);
+// 	response.payload.should.not.equal(payload);
+// 	const resPayload = response.json();
+// 	resPayload.email.should.equal(payload.email);
+// 	resPayload.petType.should.equal("Dog");
+// });
 
 teardown(() => app.close());
