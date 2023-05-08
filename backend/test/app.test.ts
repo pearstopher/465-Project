c45be9029f58fad826ef647cdbef20ac7477a68d
Pearstopher <pearstopher@gmail.com>
2023-05-08 15:21:17 -0700

    added tests for user creation and deletion

--- backend/test/app.test.ts
+++ backend/test/app.test.ts
@@ -4,7 +4,64 @@ import { test, teardown } from "tap";
 import { faker } from "@faker-js/faker";
 import app from "../src/app.js";
 
+//making these names global so I can test creating users with them
+//while also retaining the ability to update/delete the same user later
+class FakeUser {
+	id: number;
+	fName: string;
+	lName: string;
+	desc: string;
+	hidden: boolean;
+	constructor() {
+		this.id = faker.datatype.number({ min: 1000000, max: 9999999 });
+		this.fName = faker.name.firstName().toLowerCase();
+		this.lName = faker.name.lastName().toLowerCase();
+		this.desc = faker.random.words(10);
+		this.hidden = faker.datatype.boolean();
+	}
+}
+const fakeUser1 = new FakeUser();
+const fakeUser2 = new FakeUser();
+
 //Test Character CR(S)UD
+
+//S = Create
+
+test("Create a new character with valid information", async () => {
+	const payload = {
+		id: fakeUser1.id,
+		fName: fakeUser1.fName,
+		lName: fakeUser1.lName,
+		desc: fakeUser1.desc,
+		hidden: fakeUser1.hidden,
+	};
+	const response = await app.inject({
+		method: "POST",
+		url: "/character",
+		payload,
+	});
+
+	response.statusCode.should.equal(200);
+	const rj = response.json();
+	rj.fName.should.equal(fakeUser1.fName);
+});
+
+test("Create a new character with invalid information", async () => {
+	const payload = {
+		id: "id", //just throwing something in to break it,
+		fName: fakeUser2.fName,
+		lName: fakeUser2.lName,
+		desc: fakeUser2.desc,
+		hidden: fakeUser2.hidden,
+	};
+	const response = await app.inject({
+		method: "POST",
+		url: "/character",
+		payload,
+	});
+
+	response.statusCode.should.equal(500);
+});
 
 //S = Search
 
@@ -88,6 +145,35 @@ test("Search for character that exists but is hidden, by ID", async () => {
 	const rj = response.json();
 	rj.fName.should.equal("winter");
 	rj.lName.should.equal("snow");
+});
+
+// U = Update
+
+// D = Delete
+
+test("Delete a character which has beeen successfully added to the database", async () => {
+	const payload = {
+		id: fakeUser1.id,
+	};
+	const response = await app.inject({
+		method: "DELETE",
+		url: "/character",
+		payload,
+	});
+	response.statusCode.should.equal(200);
+	const rj = response.json();
+	rj.fName.should.equal(fakeUser1.fName);
+});
+test("Delete a character which does not exist in the database", async () => {
+	const payload = {
+		id: fakeUser2.id,
+	};
+	const response = await app.inject({
+		method: "DELETE",
+		url: "/character",
+		payload,
+	});
+	response.statusCode.should.equal(500);
 });
 
 // test("Creating a new user", async () => {
