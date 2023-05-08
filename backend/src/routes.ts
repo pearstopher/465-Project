2e2fb1c79e6cde01e36571b71302884871c9eeca
Pearstopher <pearstopher@gmail.com>
2023-05-08 11:16:12 -0700

    created basic character routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -31,55 +31,43 @@ async function PCPRoutes(app: FastifyInstance, _options = {}) {
 		//return req.em.find(User, { email: searchEmail });
 	});
 
-	// Core method for adding generic SEARCH http method
-	// app.route<{Body: { email: string}}>({
-	// 	method: "SEARCH",
-	// 	url: "/users",
-	//
-	// 	handler: async(req, reply) => {
-	// 		const { email } = req.body;
-	//
-	// 		try {
-	// 			const theUser = await req.em.findOne(User, { email });
-	// 			console.log(theUser);
-	// 			reply.send(theUser);
-	// 		} catch (err) {
-	// 			console.error(err);
-	// 			reply.status(500).send(err);
-	// 		}
-	// 	}
-	// });
+	// USER PROFILE
 
-	// CRUD
-	// C
-	app.post<{ Body: ICreateUsersBody }>("/users", async (req, reply) => {
-		const { name, email, petType } = req.body;
+	// USER CHARACTERS
 
-		try {
-			const newUser = await req.em.create(User, {
-				name,
-				email,
-				petType,
-			});
+	// C = Create Character
+	app.post<{ Body: { id: number; fName: string; lName: string } }>(
+		"/character",
+		async (req, reply) => {
+			const { id, fName, lName } = req.body;
 
-			await req.em.flush();
+			try {
+				const newChar = await req.em.create(Char, {
+					id,
+					fName,
+					lName,
+				});
 
-			console.log("Created new user:", newUser);
-			return reply.send(newUser);
-		} catch (err) {
-			console.log("Failed to create new user", err.message);
-			return reply.status(500).send({ message: err.message });
+				await req.em.flush();
+
+				console.log("Created new character:", newChar);
+				return reply.send(newChar);
+			} catch (err) {
+				console.log("Failed to create new user", err.message);
+				return reply.status(500).send({ message: err.message });
+			}
 		}
-	});
+	);
 
 	//READ
-	app.search("/users", async (req, reply) => {
-		const { email } = req.body;
+	// (find a character by their name)
+	app.search<{ Body: { fName: string; lName: string } }>("/character", async (req, reply) => {
+		const { fName, lName } = req.body;
 
 		try {
-			const theUser = await req.em.findOne(User, { email });
-			console.log(theUser);
-			reply.send(theUser);
+			const theChar = await req.em.findOne(Char, { fName, lName });
+			console.log(theChar);
+			reply.send(theChar);
 		} catch (err) {
 			console.error(err);
 			reply.status(500).send(err);
@@ -87,58 +75,37 @@ async function PCPRoutes(app: FastifyInstance, _options = {}) {
 	});
 
 	// UPDATE
-	app.put<{ Body: ICreateUsersBody }>("/users", async (req, reply) => {
-		const { name, email, petType } = req.body;
+	// your name or ID will be verified and won't change so you cant update those
+	// (you can delete your character and make a new one if you NEED to do this)
+	app.put<{ Body: { id: number; desc: string; hidden: boolean } }>(
+		"/character",
+		async (req, reply) => {
+			const { id, desc, hidden } = req.body;
 
-		const userToChange = await req.em.findOne(User, { email });
-		userToChange.name = name;
-		userToChange.petType = petType;
+			const charToChange = await req.em.findOne(Char, { id });
+			charToChange.desc = desc;
+			charToChange.hidden = hidden;
 
-		// Reminder -- this is how we persist our JS object changes to the database itself
-		await req.em.flush();
-		console.log(userToChange);
-		reply.send(userToChange);
-	});
+			// Reminder -- this is how we persist our JS object changes to the database itself
+			await req.em.flush();
+			console.log(charToChange);
+			reply.send(charToChange);
+		}
+	);
 
 	// DELETE
-	app.delete<{ Body: { email } }>("/users", async (req, reply) => {
-		const { email } = req.body;
+	app.delete<{ Body: { id: number } }>("/character", async (req, reply) => {
+		const { id } = req.body;
 
 		try {
-			const theUser = await req.em.findOne(User, { email });
+			const theChar = await req.em.findOne(User, { id });
 
-			await req.em.remove(theUser).flush();
-			console.log(theUser);
-			reply.send(theUser);
+			await req.em.remove(theChar).flush();
+			console.log(theChar);
+			reply.send(theChar);
 		} catch (err) {
 			console.error(err);
 			reply.status(500).send(err);
-		}
-	});
-
-	// CREATE MATCH ROUTE
-	app.post<{ Body: { email: string; matchee_email: string } }>("/match", async (req, reply) => {
-		const { email, matchee_email } = req.body;
-
-		try {
-			// make sure that the matchee exists & get their user account
-			const matchee = await req.em.findOne(User, { email: matchee_email });
-			// do the same for the matcher/owner
-			const owner = await req.em.findOne(User, { email });
-
-			//create a new match between them
-			const newMatch = await req.em.create(Match, {
-				owner,
-				matchee,
-			});
-
-			//persist it to the database
-			await req.em.flush();
-			// send the match back to the user
-			return reply.send(newMatch);
-		} catch (err) {
-			console.error(err);
-			return reply.status(500).send(err);
 		}
 	});
 }
