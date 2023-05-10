44bd8de24ed83dd519442577ffad17dca935bf65
Pearstopher <pearstopher@gmail.com>
2023-05-10 15:17:02 -0700

    pnpm checked everything

--- backend/src/db/migrations/Migration20230508183229_Initial.ts
+++ backend/src/db/migrations/Migration20230508183229_Initial.ts
@@ -1,9 +1,9 @@
-import { Migration } from '@mikro-orm/migrations';
+import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20230508183229 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('create table "characters" ("id" serial primary key, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "f_name" varchar(255) not null, "l_name" varchar(255) not null, "desc" varchar(255) null, "hidden" boolean not null);');
-  }
-
+	async up(): Promise<void> {
+		this.addSql(
+			'create table "characters" ("id" serial primary key, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "f_name" varchar(255) not null, "l_name" varchar(255) not null, "desc" varchar(255) null, "hidden" boolean not null);'
+		);
+	}
 }
