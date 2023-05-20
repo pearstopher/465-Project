f21fb377e6bb3b55d2df9f119c20d9138b0d9740
Pearstopher <pearstopher@gmail.com>
2023-05-19 21:21:58 -0700

    looking at user object

--- frontend/src/Components.tsx
+++ frontend/src/Components.tsx
@@ -39,6 +39,7 @@ export const SampleProfile = () => {
 				<img src={user.picture} alt={user.name} />
 				<h2>{user.name}</h2>
 				<p>{user.email}</p>
+				<code>{JSON.stringify(user)}</code>
 			</div>
 		)
 	);
