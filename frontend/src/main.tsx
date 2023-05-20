2fe400334a972497b319805ea7ed12195b9d0555
Pearstopher <pearstopher@gmail.com>
2023-05-19 20:25:06 -0700

    added auth0 frontend provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@css/index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const rootContainer: HTMLElement = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(rootContainer).render(
 	<React.StrictMode>
-		<App />
+		<Auth0Provider
+			domain="{yourDomain}"
+			clientId="{yourClientId}"
+			authorizationParams={{
+				redirect_uri: window.location.origin,
+			}}
+		>
+			<App />
+		</Auth0Provider>
 	</React.StrictMode>
 );
