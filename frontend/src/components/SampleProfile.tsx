89526e89e18a16cc4983653d62ca665066f24911
Pearstopher <pearstopher@gmail.com>
2023-06-07 16:53:31 -0700

    added SampleProfile.tsx

--- frontend/src/components/SampleProfile.tsx
+++ frontend/src/components/SampleProfile.tsx
@@ -1,22 +1,76 @@
-import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import React, { useEffect, useState } from "react";
 
-export const LoginButton = () => {
-	const { loginWithRedirect } = useAuth0();
+// export const SampleProfile = () => {
+// 	const { user, isAuthenticated, isLoading } = useAuth0();
+//
+// 	if (isLoading) {
+// 		return <div>Loading ...</div>;
+// 	}
+//
+// 	return (
+// 		isAuthenticated && (
+// 			<div>
+// 				<img src={user.picture} alt={user.name} />
+// 				<h2>{user.name}</h2>
+// 				<p>{user.email}</p>
+// 				<code>{JSON.stringify(user)}</code>
+// 			</div>
+// 		)
+// 	);
+// };
+
+export const SampleProfile = () => {
+	const { user, isAuthenticated, getAccessTokenSilently, getAccessTokenWithPopup } = useAuth0();
+	const [userMetadata, setUserMetadata] = useState(null);
+
+	useEffect(() => {
+		const getUserMetadata = async () => {
+			const domain = "localhost:8080";
+
+			try {
+				const accessToken = await getAccessTokenWithPopup({
+					authorizationParams: {
+						audience: `http://${domain}`,
+						scope: "read:current_user",
+					},
+				});
+				console.log(accessToken);
+
+				const userDetailsByIdUrl = `http://${domain}/api/v2/users/${user.sub}`;
+
+				const metadataResponse = await fetch(userDetailsByIdUrl, {
+					headers: {
+						Authorization: `Bearer ${accessToken}`,
+					},
+				});
+
+				const { user_metadata } = await metadataResponse.json();
+
+				//setUserMetadata(user_metadata);
+				return user_metadata;
+			} catch (e) {
+				console.log(e.message);
+			}
+		};
+
+		//getUserMetadata().then(setUserMetadata);
+	}, [getAccessTokenSilently, user?.sub]);
 
 	return (
-		<a href={"#"} onClick={() => loginWithRedirect()}>
-			Log In
-		</a>
-	);
-};
-
-export const LogoutButton = () => {
-	const { logout } = useAuth0();
-
-	return (
-		<a href={"#"} onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-			Log Out
-		</a>
+		isAuthenticated && (
+			<div>
+				<img src={user.picture} alt={user.name} />
+				<h2>{user.name}</h2>
+				<p>{user.email}</p>
+				<h3>User Metadata</h3>
+				{userMetadata ? (
+					<pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+				) : (
+					"No user metadata defined"
+				)}
+				<code>{JSON.stringify(user)}</code>
+			</div>
+		)
 	);
 };
