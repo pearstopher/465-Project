0b8abc8942de274a34807e6e43268aad2c0e84f2
Pearstopher <pearstopher@gmail.com>
2023-06-07 17:18:25 -0700

    removed old 'users list' function, unused

--- frontend/src/components/Components.tsx
+++ frontend/src/components/Components.tsx
@@ -1,11 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams, useNavigate, useLocation } from "react-router-dom";
-import { Char, CharacterResponse } from "@/PCPTypes.ts";
 import avatar from "@images/avatar.jpg";
-import portrait from "@images/portrait.jpg";
-import { useCookies } from "react-cookie";
-import { getCookie } from "@/components/AuthCallback.tsx";
 
 // login and logout buttons
 import React from "react";
@@ -23,48 +18,6 @@ export const Link = (props) => {
 	const id = props.id;
 	const name = props.name;
 	return <a href={`/character/${id}`}>{name}</a>;
-};
-
-// 1) Make a place to store the users list result
-// 2) Make the actual request to backend and store result
-// 3) Show the list of users formatted nicely in our webpage
-
-/*
- ** Hooks—functions starting with `use`—can only be called at the top level of your components
- * or [your own Hooks.](https://beta.reactjs.org/learn/reusing-logic-with-custom-hooks)**
- * You can’t call Hooks inside conditions, loops, or other nested functions. Hooks are functions,
- * but it’s helpful to think of them as unconditional declarations about your component’s needs.
- * You “use” React features at the top of your component similar to how you “import” modules
- * at the top of your file.
- */
-
-export const UsersList = () => {
-	const [users, setUsers] = useState([]);
-
-	useEffect(() => {
-		const getUsers = async () => {
-			const usersRes = await axios.get("http://localhost:8080/users");
-			return usersRes.data;
-		};
-
-		getUsers().then(setUsers);
-	}, []);
-
-	return (
-		<div>
-			<h2>Users:</h2>
-			{users ? (
-				<ul>
-					{users.map((user: { email: string; name: string }) => (
-						<li key={user.email}>
-							{" "}
-							{user.name} - {user.email}{" "}
-						</li>
-					))}
-				</ul>
-			) : null}
-		</div>
-	);
 };
 
 export const FeaturedChars = (props) => {
